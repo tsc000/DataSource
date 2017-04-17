8f9041ce61d8701659b1d4ee6c9cc740d642cdfc
童世超 <78775355@qq.com>
2017-04-17 11:23:45 +0800

    修改DataSource支持分组

--- DataSource/DataSource/DataSource/DataSource.swift
+++ DataSource/DataSource/DataSource/DataSource.swift
@@ -9,49 +9,75 @@
 import UIKit
 
 class DataSource: NSObject, UITableViewDataSource {
-
+    
+    //单个标识
     private var identifier:String = ""
     
+    //标识集合
+    private var identifierArray:[String]?
+    
+    //包含每个组
+    var itemsArray:[[AnyObject]]?
+    
+    //只包含一组
     var items:[AnyObject]?
     
     //回调事件
     private var cellBlock:((UITableViewCell, AnyObject, IndexPath)->())?
-
-    //便利初始化器
-    convenience init(items:Array<Any>, identifier:String?, cellBlock:@escaping (_ cell:UITableViewCell, _ item:AnyObject, _ indexPath:IndexPath)->()) {
-        self.init()
+    
+    //便利初始化器 单个Section
+    convenience init(items:[AnyObject], identifier:String?, cellBlock:@escaping (_ cell:UITableViewCell, _ item:AnyObject, _ indexPath:IndexPath)->()) {
+        //        self.init()
+        self.init(itemsArray: [items], identifiers: [identifier!], cellBlock: cellBlock)
         
         self.identifier = identifier!
         
         self.cellBlock = cellBlock
         
-        self.items = items as [AnyObject]
+        self.items = items
+    }
+    
+    //便利初始化器 多个Section ,兼容初始化单个Section的初始化器
+    convenience init(itemsArray:[[AnyObject]], identifiers:[String]?, cellBlock:@escaping (_ cell:UITableViewCell, _ item:AnyObject, _ indexPath:IndexPath)->()) {
+        self.init()
+        
+        self.identifierArray = identifiers
+        
+        self.cellBlock = cellBlock
+        
+        self.itemsArray = itemsArray
     }
     
     func itemAt(indexpath:IndexPath?) -> AnyObject {
-        return items![(indexpath?.row)!] as AnyObject
+        
+        return itemsArray![indexpath!.section][indexpath!.row] as AnyObject
+
     }
     
     override init() {
         super.init()
     }
-
+    
     //MARK: -UITableViewDataSource
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return itemsArray!.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items?.count ?? 0
+
+        return itemsArray![section].count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifier)!
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifierArray![indexPath.section] )!
         
         let item = itemAt(indexpath: indexPath)
         
         cellBlock!(cell, item, indexPath)
         
         return cell
+
     }
 }
--- DataSource/DataSource/ViewController.swift
+++ DataSource/DataSource/ViewController.swift
@@ -36,16 +36,28 @@ class ViewController: UIViewController{
     lazy var tableView:UITableView = {
         let tableView:UITableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height), style: .grouped)
         
-        var data = [AnyObject]()
+        var cellOneData = [AnyObject]()
         
-        for i in 0..<14 {
-            data.append("第\(i)行" as AnyObject)
+        var cellTwoData = [AnyObject]()
+        
+        for i in 0..<5 {
+            cellOneData.append("我是组一中第\(i)行的数据" as AnyObject)
         }
         
+        for i in 0..<5 {
+            cellTwoData.append("我和组一长的很像但我是组二" as AnyObject)
+        }
+        
+        
         //分离数据源
-        self.dataSource = DataSource(items: data, identifier: self.tableCellOneID, cellBlock: { (cell, item, indexPath) in
+        self.dataSource = DataSource(itemsArray: [cellOneData, cellTwoData], identifiers: [self.tableCellOneID,self.tableCellTwoID], cellBlock: { (cell, item, indexPath) in
             
-            cell.textLabel?.text = item as? String
+            //TableViewCellOne和TableViewCellTwo就是简单的继承了一下UITableViewCell
+            if indexPath.section == 0 {
+                cell.textLabel?.text = item as? String
+            } else {
+                cell.textLabel?.text = item as? String
+            }
             
         })
         
@@ -55,7 +67,7 @@ class ViewController: UIViewController{
         //注册两个cell
         tableView.register(TableViewCellOne.self, forCellReuseIdentifier: self.tableCellOneID)
         
-//        tableView.register(TableViewCellTwo.self, forCellReuseIdentifier: self.tableCellTwoID)
+        tableView.register(TableViewCellTwo.self, forCellReuseIdentifier: self.tableCellTwoID)
         
         return tableView
     }()
